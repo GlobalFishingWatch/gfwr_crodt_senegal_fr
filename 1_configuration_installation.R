02d59e6ae62a04e8dac0421daac6e7c00172bcb8
Rocio Joo <rocio.joo@globalfishingwatch.org>
2025-10-14 18:10:12 -0300

    added library and gfw_vessel_info to install script

--- 1_configuration_installation.R
+++ 1_configuration_installation.R
@@ -1,9 +1,10 @@
 # Les instructions d'installation se trouvent ici:
-# https://docs.google.com/document/d/1AyJEjwIH8ZUmUtheRpKmy0OMIWf8F_o3/edit
+# https://docs.google.com/document/d/1zf20IZHqfbRjDhQUfjorVBU0jDPV_KfB/edit
 
 #  mais nous les reprenons ici
 
-# Compte Global Fishing Watch. Créez un compte à Global Fishing Watch et un token d'accès aux APIs
+# Compte Global Fishing Watch.
+# Créez un compte sur la plateforme de Global Fishing Watch et un token d'accès aux APIs
 
 # Installez les packages nécessaires pour installer le token
 install.packages("usethis")
@@ -11,25 +12,26 @@ install.packages("usethis")
 # Pour ouvrir et éditer le fichier .R_environ:
 usethis::edit_r_environ()
 
-# Le fichier sera ouvert, copiez et collez le token:
+# Le fichier sera ouvert. Copiez et collez le token:
 #GFW_TOKEN = "eyJhb..."
-# Sauvez le fichier et recommencez la session R: (menu: Session/Restart R)
+# Sauvez le fichier et redémarrer la session R: (menu: Session/Restart R)
 
 # Installez le package depuis GitHub
 
-install.packages("remotes") # si vous n'avez pas "remotes" encore
+install.packages("remotes") # si vous n'avez pas encore "remotes"
 
 # Installez le package:
- remotes::install_github("GlobalFishingWatch/gfwr",
-                         dependencies = TRUE,
-                         build_vignettes = TRUE,
-                         ref = "develop")
+remotes::install_github("GlobalFishingWatch/gfwr",
+                        dependencies = TRUE,
+                        build_vignettes = TRUE,
+                        ref = "develop")
 
 # Pour confirmer l'installation, executez un appel aux fonctions:
 
- gfw_auth()
+library(gfwr)
+gfw_auth()
 
- get_vessel_info(431782000)
+gfw_vessel_info(431782000)
 
- # SVP fermez RStudio et recommencez si vous avez un problème.
- # Si l'erreur persiste, contactez research@globalfishingwatch.org
+# SVP fermez RStudio et redémarrez si vous avez un problème.
+# Si l'erreur persiste, contactez research@globalfishingwatch.org
